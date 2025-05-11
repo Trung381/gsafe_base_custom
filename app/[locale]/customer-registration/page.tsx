509f7890e4f4a75beb4c0b6c793955425c1eb2b8
Trung Vũ <130747543+Trung381@users.noreply.github.com>
2025-05-12 02:11:45 +0700

    haizzzz, đi ngủ mai đi học sớm 2:11 12/5

---
+++ app/[locale]/customer-registration/page.tsx
@@ -0,0 +1,9 @@
+import CustomerRegistrationForm from "@/components/customer-registration-form"
+
+export default function RegistrationPage() {
+  return (
+    <main className="min-h-screen bg-white">
+      <CustomerRegistrationForm />
+    </main>
+  )
+}
